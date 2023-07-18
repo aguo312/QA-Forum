c406532f36f91a28caf659678c004e7e930b934b
aguo312 <andrew.guo@stonybrook.edu>
2023-07-18 10:26:21 -0400

    tag rows implemented

--- client/src/components/DataTableRowTags.js
+++ client/src/components/DataTableRowTags.js
@@ -2,15 +2,21 @@ import React from "react";
 import PropTypes from "prop-types";
 
 export default class DataTableRowTags extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     const tagRows = [];
-    Array.from(this.props.tags).forEach((tagObj) => {
-      console.log(tagObj);
-    });
-    return <div>Tags</div>;
+    for (let i = 0; i < this.props.tags.length; i += 4) {
+      tagRows.push(
+        <div className="questionTags" key={i}>
+          {this.props.tags.slice(i, i + 4).map((tagObj) => {
+            return <div key={tagObj._id}>{tagObj.name}</div>;
+          })}
+        </div>
+      );
+    }
+    return <div>{tagRows}</div>;
   }
 }
+
+DataTableRowTags.propTypes = {
+  tags: PropTypes.array,
+};
